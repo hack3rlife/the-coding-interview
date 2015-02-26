8f7b8deed9281b4e79b24c47197257cb01ca4ac5
hack3rlife_cp <SND\hack3rlife_cp>
2015-02-26 06:38:52 +0000

    Adding LinkedListExtension class to include additional functionality to the Generic LinkedList<T>

---
+++ DataStructures/LinkedLists/LinkedList/LinkedListExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public static class LinkedListExtension
+    {
+        /// <summary>
+        /// Finds and returns the Node<T> which is the nth position of the LinkedList<T> 
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public static Node<T> FindNTh<T>(this LinkedList.LinkedList<T> list, int position)
+        {
+            if (list.Head != null)
+            {
+                Node<T> current = list.Head;
+
+                for (int i = 0; i < position; i++)
+                {
+                    if (current != null)
+                    {
+                        current = current.Next;
+                    }
+                    else
+                    {
+                        throw new IndexOutOfRangeException("The Linked List has no more elements");
+                    }
+                }
+
+                Node<T> nth = list.Head;
+
+                while (current != null)
+                {
+                    nth = nth.Next;
+                }
+
+                return nth;
+            }
+
+            return null;
+        }
+    }
+}
