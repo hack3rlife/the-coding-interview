bf458602b2a31d6c25de4f67b6ad265089d7ed9d
hack3rlife_cp <SND\hack3rlife_cp>
2015-03-31 07:05:58 +0000

    Moving StringReverse path and adding initial set of tests

---
+++ Strings/StringReverse/StringReverseTest/StringReverseTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.stringreverse;
+
+namespace StringReverseTest
+{
+    [TestClass]
+    public class StringReverseTest
+    {
+        [TestMethod]
+        public void ReverseTest()
+        {
+            // arrange
+            var input = "hack3rlife";
+            var expected = "efilr3kcah";
+            
+            // act
+            var actual = StringReverse.Reverse(input);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReverseOptimizedTest()
+        {
+            // arrange
+            var input = "hack3rlife";
+            var expected = "efilr3kcah";
+
+            // act
+            var actual = StringReverse.ReverseOptimized(input);
+
+            // arrange
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
