881c9aebcbe99718ad67708837d6e40f0e0dcd69
hack3rlife_cp <SND\hack3rlife_cp>
2015-03-31 06:45:58 +0000

    Given an alphanumeric phone number, print the actual number

---
+++ Strings/PhoneNumberParser/AlphanumericPhoneNumber/PhoneNumberParser.cs
@@ -0,0 +1,30 @@
+﻿
+namespace com.hack3rlife.phonenumberparser
+{
+   public  class PhoneNumberParser
+    {
+        /// <summary>
+        /// Print the actual phone number when given an alphanumeric phone number. 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static string GetPhoneNumber(string number)
+        {
+            string map = "22233344455566677778889999";
+
+            string result = string.Empty;
+
+            foreach (var n in number)
+            {
+                if (n >= 48 && n <= 57)             //[0-9]
+                    result += n;
+                else if (n >= 65 && n <= 90)        //[A-Z]
+                    result += map[n - 65];
+                else if (n >= 97 && n <= 122)        //[a-z]
+                    result += map[n - 97];
+            }
+
+            return result;
+        }      
+    }
+}
---
+++ Strings/PhoneNumberParser/AlphanumericPhoneNumberTest/PhoneNumberParserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.phonenumberparser;
+
+namespace AlphanumericPhoneNumberTest
+{
+    [TestClass]
+    public class PhoneNumberParserTest
+    {
+        /// <summary>
+        /// dial 1800-CALL-ATT (18002255288) 
+        /// </summary>
+        [TestMethod]
+        public void GetPhoneNumberTest()
+        {
+            // arrange
+            var expected = "18002255288";
+
+            // act
+            var actual = PhoneNumberParser.GetPhoneNumber("1-800-CALL-ATT");
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
