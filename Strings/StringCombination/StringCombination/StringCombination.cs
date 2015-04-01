6debbf280ad1f1e698922249148e0befc3f2a30b
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-01 09:13:46 +0000

    String combinations of a given string

---
+++ Strings/StringCombination/StringCombination/StringCombination.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace com.hack3rlife.stringcombination
+{
+    public class StringCombination
+    {
+        public static void Combine(string input, string output, int current)
+        {
+            for (int i = current; i < input.Length; i++)
+            {
+                output += input[i];
+                Console.WriteLine(output);
+                Combine(input, output, i + 1);
+                output = output.Remove(output.Length - 1);
+            }
+        }
+    }
+}
---
+++ Strings/StringCombination/StringCombinationTest/StringCombinationTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.stringcombination;
+
+namespace StringCombinationTest
+{
+    [TestClass]
+    public class StringCombinationTest
+    {
+        [TestMethod]
+        public void CombineTest()
+        {
+            // arrange
+            var input = "abc";
+
+            // act
+            StringCombination.Combine(input, string.Empty, 0);
+
+            // assert
+            Assert.IsTrue(true);
+        }
+    }
+}
