1a4115b13852ce501b08945ad91d384b359bf131
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-01 09:12:24 +0000

    String permutations of a given string

---
+++ Strings/StringPermutation/StringPermutation/StringPermutation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.stringpermutation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringPermutation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        public static void Permute(string input)
+        {
+            Permutation(input.ToCharArray(), 0);
+        }
+
+        /// <summary>
+        /// /
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="current"></param>
+        private static void Permutation(char[] input, int current)
+        {
+            if (current == input.Length - 1)
+            {
+                Console.WriteLine(input);
+                return;
+            }
+
+            for (int i = current; i < input.Length; i++)
+            {
+                input = Swap(input, current, i);
+                Permutation(input, current + 1);
+                input = Swap(input, current, i);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        private static char[] Swap(char[] input, int i, int j)
+        {
+            if (i != j)
+            {
+                input[i] ^= input[j];
+                input[j] ^= input[i];
+                input[i] ^= input[j];
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Strings/StringPermutation/StringPermutationTest/StringPermutationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.stringpermutation;
+
+namespace StringPermutationTest
+{
+    [TestClass]
+    public class StringPermutationTest
+    {
+        [TestMethod]
+        public void PermuationTest()
+        {
+            // arrange
+            var input = "ABC";
+
+            // act
+            StringPermutation.Permute(input);
+
+            // assert
+        }
+    }
+}
