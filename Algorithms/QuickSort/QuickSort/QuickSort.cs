b05f047a5e3a4d75357678d3b804d324828fbe2a
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-28 21:44:55 +0000

    QuickSort Algorithm

---
+++ Algorithms/QuickSort/QuickSort/QuickSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.quicksort
+{
+    public class QuickSort
+    {
+        public static void Sort(int[] input, int left, int right)
+        {
+            if (left < right)
+            {
+                var q = Partition(input, left, right);
+
+                Sort(input, left, q - 1);
+                Sort(input, q + 1, right);
+            }            
+        }
+
+        private static int Partition(int[] input, int left, int right)
+        {
+            var pivot = input[right]; //left + (right-left)/2
+            var storeIndex = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (input[i] < pivot)
+                {
+                    input = Swap(input, i, storeIndex++);
+                }
+            }
+
+            input = Swap(input, storeIndex, right);
+
+            return storeIndex;
+        }
+
+        private static int[] Swap(int[] input, int a, int b)
+        {
+            if (a != b)
+            {
+                input[a] ^= input[b];
+                input[b] ^= input[a];
+                input[a] ^= input[b];
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Algorithms/QuickSort/QuickSortTest/QuickSortTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.quicksort;
+
+namespace QuickSortTest
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void QSTest()
+        {
+            //Arrange
+            int[] input = new int[] { 8, 1, 3, 9, 4, 5, 0, 2, 7, 6 };
+            int[] expected = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+            //Act
+            QuickSort.Sort(input, 0, input.Length-1);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, input);
+        }
+    }
+}
