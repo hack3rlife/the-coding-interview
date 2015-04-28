20c985c26e511db639b8b7325177b8d2f3b3e4dc
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-28 07:14:08 +0000

    Dutch national flag problem

---
+++ Arrays/DutchNationalFlagProblem/DutchNationalFlagProblem/DutchNationalFlagProblem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.dnf
+{
+    public class DutchNationalFlagProblem
+    {
+        public int[] DNFSort(int[] input, int mid)
+        {
+            int i = 0;
+            int p = 0;
+            int q = input.Length - 1;
+
+            while ( i <= q )
+            {
+                if (input[i] < mid)
+                {
+                    Swap(input, i++, p++);                    
+                }
+                else if (input[i] > mid)
+                    Swap(input, i, q--);
+                else
+                    i++;
+            }
+
+            return input;
+        }
+
+        private static int[] Swap(int[] input, int a, int b)
+        {
+            if (a != b)
+            {
+                input[a] ^= input[b];
+                input[b] ^= input[a];
+                input[a] ^= input[b];
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Arrays/DutchNationalFlagProblem/DutchNationalFlagProblemTest/DutchNationalFlagProblemTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.dnf;
+
+namespace DutchNationalFlagProblemTest
+{
+    [TestClass]
+    public class DutchNationalFlagProblemTest
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            //Arrange
+            int[] input = new int[] { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+
+            //Act
+            DutchNationalFlagProblem dnf = new DutchNationalFlagProblem();
+            int[] actual = dnf.DNFSort(input, 1);
+
+            //Assert
+            int[] expected = new int[] { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2 };
+
+            CollectionAssert.AreEqual(expected, actual);            
+        }
+    }
+}
