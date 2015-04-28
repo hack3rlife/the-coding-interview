e56a183e072c8baae0d5b579630ee6626e3766b7
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-28 07:33:40 +0000

    BubbleSort Algorithm

---
+++ Algorithms/BubbleSort/BubbleSort/BubbleSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.bubblesort
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        input[i] ^= input[j];
+                        input[j] ^= input[i];
+                        input[i] ^= input[j];
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Algorithms/BubbleSort/BubbleSortTest/BubbleSortTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.bubblesort;
+
+namespace BubbleSortTest
+{
+    [TestClass]
+    public class BubbleSortTest
+    {
+        [TestMethod]
+        public void BSortTest()
+        {
+            //Arrange
+            int[] input = new int[] { 8, 1, 3, 9, 4, 5, 0, 2, 7, 6 };
+
+            //Act
+            BubbleSort bubbleSort = new BubbleSort();
+            int[] actual = bubbleSort.Sort(input);
+            int[] expected = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
