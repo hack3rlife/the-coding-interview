950cceb281f5f5a0b47fce474abcb3335e669091
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-29 07:38:55 +0000

    Find subarray with a given sum

---
+++ Arrays/SubArrayGivenSum/SubArrayGivenSum/SubArrayGivenSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.arrays
+{
+    public class SubArrayGivenSum
+    {
+
+        public static bool GetSubArray(int[] input, int sum)
+        {
+            int current_sum = 0;
+
+            int begin = 0;
+            int end = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                current_sum += input[i];
+
+                if (sum == current_sum)
+                {
+                    end = i;
+
+                    Console.WriteLine("Sum found between index {0} - {1}", begin, end);
+                    return true;
+                }
+
+                if (current_sum > sum || current_sum < 0)
+                {
+                    current_sum = 0;
+                    i = begin++;
+                    end = i;
+                }
+            }
+
+            return false;
+        }        
+
+    }
+}
---
+++ Arrays/SubArrayGivenSum/SubArrayGivenSumTest/SubArrayGivenSumTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.arrays;
+
+namespace SubArrayGivenSumTest
+{
+    [TestClass]
+    public class SubArrayGivenSumTest
+    {
+        [TestMethod]
+        public void GetSubArrayTest()
+        {
+            //Arrange
+            int[] input = { 1, 4, 20, 3, 10, 5 };
+            int sum = 33;
+
+            //Act
+           bool condition = SubArrayGivenSum.GetSubArray(input, sum);
+            
+            //Assert
+           Assert.IsTrue(condition);
+        }
+    }
+}
