adf9fa4a64e8ad8ed5048df923c4001d881e099a
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-29 06:41:01 +0000

    Find the Missing Number

---
+++ Strings/MissingElement/MissingElement/MissingElement.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hectorea.missingelement
+{
+    public class MissingElement
+    {
+
+        /// <summary>
+        /// Assuming there is no duplicates, only one value is missing in the sequence and the array is sorted
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int Find(int[] input)
+        {
+            int length = input.Length;
+            int total = (length*(length + 1)) / 2; //n(n+1)/2
+
+            for (int i = 0; i < length-1; i++)
+            {
+                total -= input[i];
+            }
+
+            return total;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <remarks>http://java67.blogspot.com/2014/12/how-to-find-missing-number-in-sorted.html</remarks>
+        public static int BinarySearch(int[] input)
+        {
+            int left = 0;
+            int right = input.Length - 1;
+            
+            while (left <= right)
+            {
+                int middle = (right + left) >> 1;
+                
+                if (input[middle] != middle)
+                {
+                    if (middle == 0 || input[middle - 1] == middle - 1)
+                    {
+                        return middle;
+                    }
+                    
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+
+            return int.MinValue;
+
+        }
+
+    }
+}
---
+++ Strings/MissingElement/MissingElementTest/MissingElementTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hectorea.missingelement;
+
+namespace MissingElementTest
+{
+    [TestClass]
+    public class MissingElementTest
+    {
+        [TestMethod]
+        public void FindMissingElementTest()
+        {
+            //Arrange
+            int[] input = new int[] { 1, 2, 3, 4, 6, 7, 8 };
+            int expected = 5;
+
+            //Act
+            int actual = MissingElement.Find(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FindMissingElementWithBinarySearchTest()
+        {
+            //Arrange
+            int[] input = new int[] { 0, 1, 2, 3, 4, 6, 7, 8 };
+            int expected = 5;
+
+            //Act
+            int actual = MissingElement.BinarySearch(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+    }
+}
