c7988293004862a0044ed5cf10f00e87236ea72d
hack3rlife_cp <SND\hack3rlife_cp>
2015-04-30 05:57:15 +0000

    Largest Sum Contiguous Subarray

---
+++ Arrays/LargestSumSubArray/LargestSumSubArray/LargestSumSubArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.arrays
+{
+    public class LargestSumSubArray
+    {
+        public static int GetLargestSum(int[] input)
+        {
+            int max_sum = 0;
+            int current_sum = 0;
+
+            int begin = 0;
+            int end = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                current_sum += input[i];
+
+                if (current_sum < 0)
+                {
+                    current_sum = 0;
+                    begin = i + 1;
+                    end = begin;
+                }
+
+                if (max_sum < current_sum)
+                {
+                    max_sum = current_sum;
+                    end = i;
+
+                    Console.WriteLine("Current Sum found between index {0} - {1}", begin, end);
+
+                }
+            }
+
+            Console.WriteLine("Max Sum: {0} found between index {1} - {2}", max_sum, begin, end);
+
+            return max_sum;
+        }
+
+    }
+}
---
+++ Arrays/LargestSumSubArray/LargestSumSubArrayTest/LargestSumSubArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.arrays;
+
+namespace LargestSumSubArrayTest
+{
+    [TestClass]
+    public class LargestSumSubArrayTest
+    {
+        [TestMethod]
+        public void GetLargestSumTest()
+        {
+            //Arrange
+            int[] input = new int[] { -2, -3, 4, -1, -2, 1, 5, -3 };
+            int expected = 7;
+
+            //Act
+            int actual = LargestSumSubArray.GetLargestSum(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
