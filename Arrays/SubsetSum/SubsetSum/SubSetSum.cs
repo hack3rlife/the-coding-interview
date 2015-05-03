d650154bd2e0aa597639ca7ae34aa8a284b9de9a
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-03 06:04:13 +0000

    SubSet Sum Problem

---
+++ Arrays/SubsetSum/SubsetSum/SubSetSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.arrays
+{
+    public static class SubSetSum
+    {
+        //using dynamic programming
+        public static bool HasSubSetSum(int[] input, int n, int sum)
+        {
+            if (sum == 0)
+                return true;
+            if (n == 0 && sum != 0)
+                return false;
+
+            if (input[n - 1] > sum)
+                return HasSubSetSum(input, n - 1, sum);
+
+            return HasSubSetSum(input, n - 1, sum) || HasSubSetSum(input, n - 1, sum - input[n - 1]);
+        }
+
+    }
+}
---
+++ Arrays/SubsetSum/SubsetSumTest/SubSetSumTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.arrays;
+
+namespace SubsetSumTest
+{
+    [TestClass]
+    public class SubSetSumTest
+    {
+        [TestMethod]
+        public void GetSubSetSumTest()
+        {
+            //Arrange
+            int[] input = new int[] { 3, 34, 4, 12, 5, 2 };
+
+            //Act
+            bool condition = SubSetSum.HasSubSetSum(input, input.Length, 7);
+
+            //Assert
+            Assert.IsTrue(condition);
+        }
+    }
+}
