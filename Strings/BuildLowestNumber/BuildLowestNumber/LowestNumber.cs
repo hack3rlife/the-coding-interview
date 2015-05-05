e5899341133982834ee18df05762ed58380cdde2
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-05 07:45:00 +0000

    Build Lowest Number by Removing n digits

---
+++ Strings/BuildLowestNumber/BuildLowestNumber/LowestNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.strings
+{
+    public  class LowestNumber
+    {
+        public static void BuildLowestNumber(string input, int n, ref string result)
+        {
+            if (n == 0)
+            {
+                 result += input;
+                 return;
+            }
+
+            int length = input.Length-1;
+
+            if( length <= n)
+            {
+                 result = string.Empty;
+                 return;
+            }
+
+            int index = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (input[i] < input[index]) 
+                    index = i;            
+            }            
+
+            result += input[index];
+
+            string substring = input.Substring(index + 1, length - index);
+
+            Console.WriteLine("result: {0} - substring: {1}", result, substring);
+
+            BuildLowestNumber(substring, n - index, ref result);
+        }
+    }
+}
---
+++ Strings/BuildLowestNumber/BuildLowestNumberTest/BuildLowestNumberTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace BuildLowestNumberTest
+{
+    [TestClass]
+    public class BuildLowestNumberTest
+    {
+        [TestMethod]
+        public void LowestNumberBuildTest()
+        {
+            //Arrange
+            string input = "4325043";
+            string expected = "2043";
+           string actual = string.Empty;
+
+            int n = 3;            
+
+            //Act
+            LowestNumber.BuildLowestNumber(input, n, ref actual);
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
