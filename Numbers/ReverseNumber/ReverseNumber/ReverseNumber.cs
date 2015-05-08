f224bac555e50097886faa263bf9b6a65f4dfbad
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-08 07:49:38 +0000

    Write a method to reverse a number

---
+++ Numbers/ReverseNumber/ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.numbers
+{
+    public class ReverseNumber
+    {
+        public static int Reverse(int number)
+        {
+            int quotient = number;
+            int remainder = 0;
+            int result = 0;
+
+            do
+            {
+                remainder = quotient % 10;
+                quotient /= 10;
+
+                result = (result * 10) + remainder;
+
+            } while (quotient > 0);
+
+            return result;
+        }
+
+    }
+}
---
+++ Numbers/ReverseNumber/ReverseNumberTest/ReverseNumberTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.numbers;
+
+namespace ReverseNumberTest
+{
+    [TestClass]
+    public class ReverseNumberTest
+    {
+        [TestMethod]
+        public void ReverseTest()
+        {
+            // Arrange
+            int input = 1234;
+
+            // Act
+            int actual = ReverseNumber.Reverse(input);
+            int expected = 4321;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
