23b0d41f4331ef1e88ee18fc2f85086ff94256ca
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-08 07:31:02 +0000

    Concatenate two integer values

---
+++ Numbers/ConcatenateTwoNumbers/ConcatenateTwoNumbers/ConcatenateTwoNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.numbers
+{
+    public class ConcatenateTwoNumbers
+    {
+        /// <summary>
+        /// Concatenate two numbers
+        /// </summary>
+        /// <param name="a">Integer value</param>
+        /// <param name="b">Integer value</param>
+        /// <returns>The sum of the two numbers</returns>
+        public static int Concatenate(int number1, int number2)
+        {
+            int c = number2;
+            while (c > 0)
+            {
+                number1 *= 10;
+                c /= 10;
+            }
+
+            return number1 + number2;
+        }
+
+    }
+}
---
+++ Numbers/ConcatenateTwoNumbers/ConcatenateTwoNumbersTest/ConcatenateTwoNumbersTest/ConcatenateTwoNumbersTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.numbers;
+
+namespace ConcatenateTwoNumbersTest
+{
+    [TestClass]
+    public class ConcatenateTwoNumbersTest
+    {
+        [TestMethod]
+        public void ConcatenateTest()
+        {
+            // Arrange
+            Random r = new Random();
+            int num1 = r.Next(10000);
+            int num2 = r.Next(10000);
+
+            // Act
+            int actual = ConcatenateTwoNumbers.Concatenate(num1, num2);
+            int expected = num1*1000 + num2;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
