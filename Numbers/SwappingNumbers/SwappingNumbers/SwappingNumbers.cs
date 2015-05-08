bd74d742b87cf7058c9894e02a56e627908cd4a6
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-08 08:24:06 +0000

    Swap two variables without using a third variable.

---
+++ Numbers/SwappingNumbers/SwappingNumbers/SwappingNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.numbers
+{
+    public class SwappingNumbers
+    {
+        /// <summary>
+        /// Using bitwise operations (XOR)
+        /// </summary>
+        /// <param name="a">Integer Value</param>
+        /// <param name="b">Integer Value</param>
+        public static void BitwiseSwap(ref int a, ref int b)
+        {
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+
+        /// <summary>
+        /// Using simple addition and subtraction operations
+        /// </summary>
+        /// <param name="a">Interger Value</param>
+        /// <param name="b">Integer Value</param>
+        public static void ArithmeticSwap(ref int a, ref int b)
+        {
+            a = a + b;  //Stores temporally the value of b in a
+            b = a - b;  //Removes the value of b and we get the value of a in b
+            a = a - b;  //Removes the value of a and we get the value of b in a
+            // Got it? A little bit tricky, he?
+        }        
+    }
+}
---
+++ Numbers/SwappingNumbers/SwappingNumbersTest/SwappingNumbersTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.numbers;
+
+namespace SwappingNumbersTest
+{
+    [TestClass]
+    public class SwappingNumbersTest
+    {
+        [TestMethod]
+        public void SwapTest()
+        {
+            // Arrange
+            int a = 10;
+            int b = 5;
+
+            int d = 10;
+            int e = 5;
+
+            // Act
+            SwappingNumbers.BitwiseSwap(ref a, ref b);
+            SwappingNumbers.BitwiseSwap(ref d, ref e);
+
+            // Assert
+            Assert.AreEqual(a, d);
+            Assert.AreEqual(b, e);
+            
+        }
+
+        [TestMethod]
+        public void BitwiseSwapTest()
+        {
+            // Arrange
+            int num1 = 10;
+            int num2 = 5;
+
+            // Act
+            SwappingNumbers.BitwiseSwap(ref num1, ref num2);
+
+            // Assert
+            Assert.AreEqual(5, num1);
+            Assert.AreEqual(10, num2);
+        }
+
+        [TestMethod]
+        public void ArithmeticSwapTest()
+        {
+            // Arrange
+            int num1 = 10;
+            int num2 = 5;
+
+            // Act
+            SwappingNumbers.ArithmeticSwap(ref num1, ref num2);
+
+            // Assert
+            Assert.AreEqual(5, num1);
+            Assert.AreEqual(10, num2);
+        }
+    }
+}
