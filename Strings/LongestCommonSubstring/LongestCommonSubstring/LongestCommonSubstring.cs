38579f9674b277bfd00290e618ee835643c0f5d2
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-08 05:37:36 +0000

    Longest Common Substring

---
+++ Strings/LongestCommonSubstring/LongestCommonSubstring/LongestCommonSubstring.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.strings
+{
+    public class LongestCommonSubstring
+    {
+        public static int GetLCS(string input1, string input2)
+        {
+            int m = input1.Length;
+            int n = input2.Length;
+
+            int[,] num = new int[m, n];
+
+            int result = 0;
+
+
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    if(input1[i] == input2[j])
+                    {
+                        num[i, j] = num[i-1, j-1] + 1;
+
+                        result = (result > num[i, j]) ? result : num[i, j];
+                    }
+                    else
+                    {
+                        num[i, j] = 0;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Strings/LongestCommonSubstring/LongestCommonSubstringTest/LongestCommonSubstringTest/LongestCommonSubstringTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace LongestCommonSubstringTest
+{
+    [TestClass]
+    public class LongestCommonSubstringTest
+    {
+        [TestMethod]
+        public void GetLcsTest()
+        {
+            //Arrange
+            string input1 = "OldSite:GeeksforGeeks.org";
+            string input2 = "NewSite:GeeksQuiz.com";
+
+            int expected = 10;
+
+            //Act
+            int actual = LongestCommonSubstring.GetLCS(input1, input2);
+
+            //Test
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
