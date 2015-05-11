919dd6ef34387709ce777f7675aca358afd3d914
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-11 05:42:11 +0000

    Factorial Number of a given number

---
+++ Numbers/FactorialNumber/FactorialNumber/FactorialNumber.cs
@@ -0,0 +1,15 @@
+﻿
+namespace com.hack3rlife.numbers
+{
+    public class FactorialNumber
+    {
+        public static int Factorial(int number)
+        {
+            if (number == 1)
+                return 1;
+            else
+                return Factorial(number - 1) * number;
+        }
+
+    }
+}
---
+++ Numbers/FactorialNumber/FactorialNumberTest/FactorialNumberTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.numbers;
+
+namespace FactorialNumberTest
+{
+    [TestClass]
+    public class FactorialNumberTest
+    {
+        [TestMethod]
+        public void FactorialTest()
+        {
+            //Arrange
+            int input = 5;
+
+            //Act
+            int actual = FactorialNumber.Factorial(input);
+            int expected = 120;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
