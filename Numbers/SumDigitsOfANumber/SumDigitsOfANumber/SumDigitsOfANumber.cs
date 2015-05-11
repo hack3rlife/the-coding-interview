324350ff1ecf806a97d032f7f2b266f8f7cdf471
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-11 04:12:02 +0000

    SumDigits of a Number

---
+++ Numbers/SumDigitsOfANumber/SumDigitsOfANumber/SumDigitsOfANumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.numbers
+{
+    public class SumDigitsOfANumber
+    {
+        public static int SumDigits(int number)
+        {
+            int result;
+
+            do
+            {
+                result = 0;
+
+                while (number > 0)
+                {
+                    result += number % 10;
+                    number /= 10;
+                }
+
+                number = result;
+
+            } while (number > 10);
+
+
+            return result;
+        }
+    }
+}
---
+++ Numbers/SumDigitsOfANumber/SumDigitsOfANumberTest/SumDigitsOfANumberTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.numbers;
+
+namespace SumDigitsOfANumberTest
+{
+    [TestClass]
+    public class SumDigitsOfANumberTest
+    {
+        [TestMethod]
+        public void SumDigitsTest()
+        {
+            //Arrange
+            int input = 12345;            
+
+            //Act
+            int actual = SumDigitsOfANumber.SumDigits(input);
+             int expected = 6;
+
+            //Assert
+             Assert.AreEqual(expected, actual);
+        }
+    }
+}
