23f4553e49666fdd97f03d6ecbbb2fd74492a352
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-12 07:25:34 +0000

    Verify if a given string is a palindrome

---
+++ Strings/IsPalindrome/IsPalindrome/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿namespace com.hack3rlife.strings
+{
+    public class Palindrome
+    {
+        /// <summary>
+        ///  Check whether a string is palindrome or not
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <returns>True if the string is palindrome; false in another case</returns>
+        public static bool IsPalindrome( string input)
+        {
+            if (input == string.Empty || input.Length <= 1)
+                return false;
+
+            int i = 0, j = input.Length - 1;
+            while (i < j)
+            {
+                //ignore blank spaces
+                while (i < input.Length && input[i] == ' ')
+                    i++;
+                while (j >= 0 && input[j] == ' ')
+                    j--;
+
+                if (i >= input.Length || j < 0 || input[i] != input[j])
+                    return false;
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Strings/IsPalindrome/IsPalindromeTest/PalindromeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace IsPalindromeTest
+{
+    [TestClass]
+    public class PalindromeTest
+    {
+        [TestMethod]
+        public void IsPalindromeTestPass()
+        {
+            //Arrange
+            string input = "radar";
+
+            //Act
+            bool condition = Palindrome.IsPalindrome(input);
+
+            //Assert 
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestFail()
+        {
+            //Arrange
+            string input = "radars";
+
+            //Act
+            bool condition = Palindrome.IsPalindrome(input);
+
+            //Assert 
+            Assert.IsFalse(condition);
+        }
+    }
+}
