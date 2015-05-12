ce1d173b46d133d7adf64eaece033b17dc483ad4
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-12 08:30:14 +0000

    Verify if two strings are rotations of each other or not

---
+++ Strings/IsRotation/IsRotation/Rotation.cs
@@ -0,0 +1,39 @@
+﻿namespace com.hack3rlife.strings
+{
+    public class Rotation
+    {
+        public static bool IsRotation(string source, string target)
+        {
+            bool result = false;
+
+            if (source.Length != target.Length)
+                return result;
+
+            source += source;
+
+            int i = 0, count = 0;
+            while (i < source.Length )
+            {
+                if (source[i] == target[count])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+                if (count == target.Length)
+                {
+                    result = true;
+
+                    break;
+                }
+
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Strings/IsRotation/IsRotationTest/RotationTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace IsRotationTest
+{
+    [TestClass]
+    public class RotationTest
+    {
+        [TestMethod]
+        public void IsRotationTestPassed()
+        {
+            //Arrange
+            string source = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget lorem sagittis, pretium dolor eget, fermentum dolor. In sed.";
+            string target = " consectetur adipiscing elit. Morbi eget lorem sagittis, pretium dolor eget, fermentum dolor. In sed.Lorem ipsum dolor sit amet,";
+            
+
+            //Act
+            bool condition = Rotation.IsRotation(source,target);
+
+            //Assert
+            Assert.IsTrue(condition);
+        }
+    }
+}
