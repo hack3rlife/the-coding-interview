4fdfc39538402af89744a8f17635b5733b903702
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-12 06:00:36 +0000

    Prime Numbers

---
+++ Numbers/PrimeNumbers/PrimeNumbers/PrimeNumbers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.numbers
+{
+    public class PrimeNumbers
+    {
+        /// <summary>
+        /// Checks if a number is prime or not using Prmality Test
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>True if the number is prime; otherwise false</returns>
+        /// <see cref="http://en.wikipedia.org/wiki/Primality_test"/>
+        public static bool isPrimeNumber(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            else if (number <= 3)
+            {
+                return true;
+            }
+            else if (number % 2 == 0 || number % 3 == 0)
+            {
+                return false;
+            }
+
+            int i = 5;
+
+            while (i * i <= number)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                {
+                    return false;
+                }
+
+                i += 6;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Prints all primes number smaller than or equal to number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <see cref=">http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes"/>
+        public static void SieveOfEratosthenes(int number)
+        {
+            bool[] arr = Enumerable.Repeat(true, number).ToArray();
+
+            for (int i = 1; i < number; i++)
+            {
+                if (arr[i] == true)
+                {
+                    int j = i + 1;
+
+                    Console.Write(" {0} ", j);
+
+                    int k = 2;
+                    int num;
+
+                    while ((num = j * k) <= number)
+                    {
+                        arr[num - 1] = false;
+                        k++;
+                    }
+                }
+            }           
+        }
+
+    }
+}
---
+++ Numbers/PrimeNumbers/PrimeNumbersTest/PrimeNumbersTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.numbers;
+
+namespace PrimeNumbersTest
+{
+    [TestClass]
+    public class PrimeNumbersTest
+    {
+        [TestMethod]
+        public void isPrimeNumberTest()
+        {
+            //Arrange
+            int number = 23;
+
+            //Act
+            bool condition = PrimeNumbers.isPrimeNumber(number);
+
+            //Assert
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void SieveOfEratosthenesTest()
+        {
+            //Arrange
+            int number = 30;
+
+            //Act
+           PrimeNumbers.SieveOfEratosthenes(number);
+
+            //Assert
+        }
+    }
+}
