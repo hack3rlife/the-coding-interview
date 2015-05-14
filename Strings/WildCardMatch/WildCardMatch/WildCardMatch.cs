d82e997a6409edd495f7a94ccbe6b31e16552f3f
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-14 07:46:04 +0000

    Wildcard Match

---
+++ Strings/WildCardMatch/WildCardMatch/WildCardMatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace com.hackerlife.strings
+{
+    public class WildCardMatch
+    {
+        public static bool IsMatch(string input, string wildcard)
+        {
+            int i = 0;
+            int j = 0;
+
+
+            while (i < wildcard.Length && j < input.Length)
+            {
+                if (wildcard[i] == '*')
+                {
+                    if (i + 1 < wildcard.Length)
+                    {
+                        if (wildcard[i + 1] != input[j])
+                            j++;
+                        else
+                            i++;
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+                else if (wildcard[i] == '?' || wildcard[i] == input[j])
+                {
+                    i++;
+                    j++;
+                }
+                else if (wildcard[i] != input[j])
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Strings/WildCardMatch/WildCardMatchTest/WildCardMatchTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hackerlife.strings;
+
+namespace WildCardMatchTest
+{
+    [TestClass]
+    public class WildCardMatchTest
+    {
+        [TestMethod]
+        public void IsMatchTestPass()
+        {
+            // Arrange
+            string input = "geeks";
+            string wildcard = "g*ks";
+
+            // Act
+            bool condition = WildCardMatch.IsMatch(input, wildcard);
+
+            // Assert
+            Assert.IsTrue(condition);
+        }
+
+        [TestMethod]
+        public void IsMatchTestFail()
+        {
+            // Arrange
+            string input = "abcd";
+            string wildcard = "abc*c?d";
+
+            // Act
+            bool condition = WildCardMatch.IsMatch(input, wildcard);
+
+            // Assert
+            Assert.IsTrue(condition);
+        }
+    }
+}
