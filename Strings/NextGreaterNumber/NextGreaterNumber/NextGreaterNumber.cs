3c38917bcde551114dcbac0f519f89baa72bae89
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-14 06:11:41 +0000

    Next greater number with same set of digits

---
+++ Strings/NextGreaterNumber/NextGreaterNumber/NextGreaterNumber.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace com.hack3rlife.strings
+{
+    public class NextGreaterNumber
+    {
+        public static string FindNext(string number)
+        {
+            string result = string.Empty;
+            int i;
+
+            //from right to left, find the first digit that is
+            // smaller than the digit next to it
+            for (i = number.Length-1; i > 0; i--)
+            {
+                if (number[i] > number[i - 1])
+                    break;
+            }
+
+            if (i == 0)
+                return string.Empty;
+
+            //find the smallest digit on right side of (i-1)'th digit that is
+            // greater than number[i-1]
+            int indexSmallest = i;
+            for (int j = i+1; j < number.Length; j++)
+            {
+                if (number[j] > number[i - 1] &&
+                    number[j] < number[indexSmallest])
+                {
+                    indexSmallest = j;
+                }
+            }
+
+            result = Swap(number.ToCharArray(0, number.Length), i-1, indexSmallest);
+
+
+            result = Sort(result.ToCharArray(0, result.Length), i, result.Length);
+
+            return result;
+                
+        }
+
+        private static string Swap(char[] input, int i, int j)
+        {
+            if (i != j)
+            {
+                input[i] ^= input[j];
+                input[j] ^= input[i];
+                input[i] ^= input[j];
+            }
+
+            return new string(input);
+        }
+
+        private static string Sort(char[] input, int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                for (int j = i + 1; j < end; j++)
+                {
+                    if (input[i] > input[j])
+                    {
+                        input[i] ^= input[j];
+                        input[j] ^= input[i];
+                        input[i] ^= input[j];
+                    }
+                }
+            }
+
+            return new string(input);
+        }
+    }
+}
---
+++ Strings/NextGreaterNumber/NextGreaterNumberTest/NextGreaterNumberTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace NextGreaterNumberTest
+{
+    [TestClass]
+    public class NextGreaterNumberTest
+    {
+        [TestMethod]
+        public void FindNextTest()
+        {
+            //Arrange
+            string number = "534976";
+            
+            //Act
+            string actual = NextGreaterNumber.FindNext(number);
+            string expected = "536479";
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
