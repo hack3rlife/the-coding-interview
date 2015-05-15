ccdf163e3e5a53290bed708365f13f31157ffa0a
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-15 16:36:13 +0000

    Naive Pattern Searching

---
+++ Strings/PatternSearching/PatternSearching/NaivePatternSearching.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.strings
+{
+    public class NaivePatternSearching
+    {
+        public static int Search(string pattern, string input)
+        {
+            int patternLength = pattern.Length;
+            int inputLength = input.Length;
+
+            for (int i = 0; i < inputLength-patternLength; i++)
+            {
+                for (int j = 0; j < patternLength; j++)
+                {
+                    if (input[i + j] != pattern[j])
+                    {
+                        break;
+                    }
+
+                    if (j == patternLength-1)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Strings/PatternSearching/PatternSearchingTest/NaivePatternSearchingTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace PatternSearchingTest
+{
+    [TestClass]
+    public class NaivePatternSearchingTest
+    {
+        [TestMethod]
+        public void SearchTest()
+        {
+            //Arrange
+            string input = "My name is Hector";
+            string pattern = "name";
+
+            //Act
+            int actual = NaivePatternSearching.Search(pattern, input);
+            int expected = 3;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
