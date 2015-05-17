5e756183906b2899dc5bfdf255f4b4ff714afb9c
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-17 04:53:09 +0000

    Searching for Patterns: KMP Algorithm

---
+++ Strings/PatternSearching/PatternSearching/KMPAlgorithm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace com.hack3rlife.strings
+{
+    public class KMPAlgorithm
+    {
+        static int[] lps;
+
+        public static int Search(string pattern, string input)
+        {
+            int patternLength = pattern.Length;
+            int inputLength = input.Length;
+
+            int index = -1;
+
+            CalculateLpsArray(pattern);
+
+            int i = 0;
+            int j =0;
+
+            while (i < inputLength)
+            {
+                if (input[i] == pattern[j])
+                {
+                    i++;
+                    j++;
+                }
+
+                if (j == patternLength - 1)
+                {
+                    Console.WriteLine("Found pattern at index: {0}", i - j);
+                    index = i - j;
+                    j = lps[j - 1];
+                }
+                else if (i < inputLength && input[i] != pattern[j])  // mismatch after j matches
+                {
+                    if (j != 0)
+                        j = lps[j - 1];
+                    else
+                        i = i + 1;
+                }
+            }
+
+
+            return index;
+        }
+
+        private static void CalculateLpsArray(string pattern)
+        {
+            int length = 0;
+            int i = 1;
+
+            lps = new int[pattern.Length];
+            lps[0] = 0;
+
+            while (i < pattern.Length)
+            {
+                if (pattern[i] == pattern[length])
+                {
+                    lps[i++] = ++length;
+                }
+                else
+                {
+                    if (length != 0)
+                    {
+                        length = lps[length - 1];
+                    }
+                    else
+                    {
+                        lps[i++] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Strings/PatternSearching/PatternSearchingTest/KMPAlgorithmTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.strings;
+
+namespace PatternSearchingTest
+{
+    [TestClass]
+    public class KMPAlgorithmTest
+    {
+        [TestMethod]
+        public void SearchTest()
+        {
+            //Arrange
+            string input = "AAAAAAAAAAAAAAAAAB";
+            string pattern = "AAAAB";
+
+            //Act
+            int actual = KMPAlgorithm.Search(pattern, input);
+            int expected = 13;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
