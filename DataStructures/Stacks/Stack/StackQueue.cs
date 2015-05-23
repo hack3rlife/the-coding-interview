1953230c076e0e4583ef089db9a5ff1c11431ec4
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-23 08:35:32 +0000

    Implement a Stack using a Queue

---
+++ DataStructures/Stacks/Stack/StackQueue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.datastructures
+{
+    /// <summary>
+    /// Follow-up: Implment a Stack using a Queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class StackQueue<T>
+    {
+        private Queue _top;
+        public int Count { get; private set; }
+
+        public StackQueue()
+        {
+            _top = new Queue();
+            Count = 0;
+        }
+
+        public void Push(int value)
+        {
+            _top.Enqueue(value);
+            Count++;
+        }
+
+        public int Pop()
+        {
+            int value = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                value = (int)_top.Dequeue();
+                _top.Enqueue(value);
+            }
+            Count--;
+            return value;
+        }
+
+    }
+}
---
+++ DataStructures/Stacks/StackTest/StackQueueTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.datastructures;
+
+namespace StackTest
+{
+    [TestClass]
+    public class StackQueueTest
+    {
+        [TestMethod]
+        public void PushTest()
+        {
+            //Arrange
+            StackQueue<int> stack = new StackQueue<int>();
+
+            //Act
+            stack.Push(0);
+            stack.Push(1);
+            stack.Push(2);
+
+            //Assert
+            Assert.IsTrue(stack.Count == 3);
+        }
+
+        [TestMethod]
+        public void PopTest()
+        {
+            //Arrange
+            StackQueue<int> stack = new StackQueue<int>();
+            stack.Push(0);
+            stack.Push(1);
+            stack.Push(2);
+
+            //Act
+            int actual = stack.Pop();
+            int expected = 2;
+
+            //Asset
+            Assert.AreEqual<int>(expected, actual);
+        }
+    }
+}
