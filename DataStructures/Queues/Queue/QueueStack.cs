76dd777087a04daa324595aaf8d9ae8b66bb0cb4
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-24 19:34:48 +0000

    Implement a Queue using a Stack

---
+++ DataStructures/Queues/Queue/QueueStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.queue
+{
+    public class QueueStack<T> 
+    {
+        private Stack<T> input;
+        private Stack<T> output;
+
+        public int Count { get; set; }
+
+        public QueueStack()
+        {
+            input = new Stack<T>();
+            output = new Stack<T>();
+
+            Count = 0;
+        }
+
+        public void Enqueue(T value)
+        {
+            input.Push(value);
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (Count > 0)
+            {
+                T value = input.Pop();
+                output.Push(value);
+               
+
+                for (int i = 0; input.Count>0; i++)
+                {
+                    value = input.Pop();
+                    output.Push(value);
+                 
+                }
+
+                for (int i = 0; output.Count>0; i++)
+                {
+                    input.Push(output.Pop());
+                }
+
+                Count--;
+                return value;
+            }
+
+            throw new NullReferenceException("Queue is empty");
+        }
+    }
+}
---
+++ DataStructures/Queues/QueueTest/QueueStackTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.queue;
+
+namespace QueueTest
+{
+    [TestClass]
+    public class QueueStackTest
+    {
+        [TestMethod]
+        public void EnqueueTest()
+        {
+            //Arrange
+            QueueStack<int> q = new QueueStack<int>();
+
+            //Act
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+
+            //Assert
+            Assert.AreEqual(3, q.Count);
+        }
+
+        [TestMethod]
+        public void DequeueTest()
+        {
+            //Arrange
+            QueueStack<int> q = new QueueStack<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue(5);
+
+            //Act
+            
+           int actual = q.Dequeue();
+           int expected = 1;
+
+            //Assert
+           Assert.AreEqual<int>(expected,actual);
+            Assert.AreEqual(4, q.Count);
+        }
+    }
+}
