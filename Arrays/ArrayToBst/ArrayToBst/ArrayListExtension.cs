e137a865f846630448a2e7a23e36f2e8a71d5de5
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-25 10:13:08 +0000

    Sorted Array to Balanced BST

---
+++ Arrays/ArrayToBst/ArrayToBst/ArrayListExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using com.hack3rlife.datastructures;
+using System.Collections.Generic;
+
+namespace com.hack3rlife.arrays
+{
+    public static class ListExtension
+    {
+        public static BinarySearchTree<int> ToBinarySearchTree(this IList<int> list, int start, int end)
+        {
+            var root = ToBinarySearchTreeNode(list, start,end);
+            var bst = new BinarySearchTree<int>(root);
+            return bst;
+        }
+
+        private static BinarySearchTreeNode<int> ToBinarySearchTreeNode(this IList<int> list, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            int mid = (start + end) >> 1;
+
+            var root = new BinarySearchTreeNode<int>(list[mid]);
+
+            var left = ToBinarySearchTreeNode(list, start, mid - 1);
+            root.Left = left;
+
+            var right = ToBinarySearchTreeNode(list, mid + 1, end);
+            root.Right = right;
+
+            return root;
+        }
+
+    }
+}
---
+++ Arrays/ArrayToBst/ArrayToBstTest/ArrayToBinarySearchTreeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.arrays;
+using com.hack3rlife.datastructures;
+
+namespace ArrayToBstTest
+{
+    [TestClass]
+    public class ArrayToBinarySearchTreeTest
+    {
+        [TestMethod]
+        public void ArrayToBinarySearchTreeTestPass()
+        {
+            //Arrange
+            var expected = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            //Act
+            var bst = expected.ToBinarySearchTree(0, expected.Length-1);
+            bst.Display();
+
+            //Assert
+            int i = 0;
+            foreach (var actual in bst.InOrderEnumerable)
+            {
+                Assert.AreEqual<int>(expected[i++], actual);
+            }
+        }
+    }
+}
