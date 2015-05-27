86ffa3d7a3220476661af44c945f56ba1f741eb3
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-27 08:23:40 +0000

    Search an element in a sorted and pivoted array

---
+++ Arrays/BinarySearch/BinarySearch/BinarySearch.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.arrays
+{
+    public class BinarySearch
+    {
+        public static int? StandardBinarySearchIterative(int[] input, int key, int min, int max)
+        {
+            while (max > min)
+            {
+                int mid = min + ((max - min) / 2);
+
+                if (input[mid] < key)
+                {
+                    min = mid + 1;
+                }
+                else if (input[mid] > key)
+                    max = mid - 1;
+                else
+                    return mid;
+            }
+
+            return null;
+        }
+
+        public static int? StandardBinarySearchRecursive(int[] input, int key, int min, int max)
+        {
+            if (max < min)
+                return null;
+
+            int mid = min + ((max - min) / 2);
+
+            if (key == input[mid])
+                return mid;
+            if (key > input[mid])
+                return StandardBinarySearchRecursive(input, key, (mid + 1), max);
+            else
+                return StandardBinarySearchRecursive(input, key, min, (mid - 1));
+        }
+
+        public static int? PivotedBinarySearch(int[] input, int key)
+        {
+            int pivot = FindPivot(input, 0, input.Length - 1);
+
+            // No rotation, we use standard binary search
+            if (pivot == -1)
+                return StandardBinarySearchRecursive(input, key, 0, input.Length - 1);
+
+            // If we found a pivot, then first compare with pivot and then
+            // search in two subarrays around pivot
+            if (input[pivot] == key)
+                return pivot;
+
+            if (input[0] <= key)
+                return StandardBinarySearchRecursive(input, key, 0, pivot - 1);
+            else
+                return StandardBinarySearchRecursive(input, key, pivot + 1, input.Length - 1);
+        }
+
+        private static int FindPivot(int[] input, int low, int high)
+        {
+            if (high < low) return -1;
+
+            if (high == low) return low;
+
+            int mid = low + (high - low) / 2;
+
+            if (mid < high && input[mid] > input[mid + 1])
+                return mid;
+
+            if (mid > low && input[mid] < input[mid - 1])
+                return (mid - 1);
+
+            if (input[low] >= input[mid])
+                return FindPivot(input, low, mid - 1);
+            else
+                return FindPivot(input, mid + 1, high);
+        }
+    }
+}
---
+++ Arrays/BinarySearch/BinarySearchTest/BunarySearchTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.arrays;
+
+namespace BinarySearchTest
+{
+    [TestClass]
+    public class BunarySearchTest
+    {
+        [TestMethod]
+        public void StandardBunarySearchTest()
+        {
+            //Arrange
+            int[] input = new int[] { 1,2,3,4,5 };
+            
+            //Act
+            int? actual = BinarySearch.StandardBinarySearchIterative(input, 4, 0, input.Length - 1);
+            int? expected = BinarySearch.StandardBinarySearchRecursive(input, 4, 0, input.Length - 1);
+            //Assert
+            Assert.AreEqual<int?>(expected, actual);
+        }
+
+         [TestMethod]
+        public void PivotedBinarySearchTest()
+        {
+            //Arrange
+            int[] input = new int[] { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+            
+            //Act
+            int? actual = BinarySearch.PivotedBinarySearch(input, 7);
+            int? expected = 2;
+            //Assert
+            Assert.AreEqual<int?>(expected, actual);
+        }
+    }
+}
