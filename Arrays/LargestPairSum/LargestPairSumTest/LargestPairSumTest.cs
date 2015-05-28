1613a53562128ce7875255d063db8efc91aafeba
hack3rlife_cp <SND\hack3rlife_cp>
2015-05-28 21:24:16 +0000

    Largest pair sum in an unsorted array

---
+++ Arrays/LargestPairSum/LargestPairSumTest/LargestPairSumTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.arrays;
+
+namespace LargestPairSumTest
+{
+    [TestClass]
+    public class LargestPairSumTest
+    {
+        [TestMethod]
+        public void LargestPairSumFindTest()
+        {
+            //Arrange
+            int[] input = { 12, 34, 10, 6, 40 };
+
+            //Act
+            int actual = LargestPairSum.Find(input);
+            int expected = 74;
+
+            //Assert
+            Assert.AreEqual<int>(expected, actual);
+        }
+    }
+}
