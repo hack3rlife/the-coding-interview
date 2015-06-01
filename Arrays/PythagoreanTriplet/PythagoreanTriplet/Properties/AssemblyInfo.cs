00548f8edb403f6404f2f1e2ede76e239c3ececb
hack3rlife_cp <SND\hack3rlife_cp>
2015-06-01 16:51:37 +0000

    Count the number of possible triangles

--- Arrays/PythagoreanTriplet/PythagoreanTriplet/Properties/AssemblyInfo.cs
+++ Arrays/PythagoreanTriplet/PythagoreanTriplet/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TriangleTriplets")]
+[assembly: AssemblyTitle("PythagoreanTriplet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TriangleTriplets")]
+[assembly: AssemblyProduct("PythagoreanTriplet")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("08924593-d87e-46ce-b717-dbabc5a9e5d4")]
+[assembly: Guid("f90e70be-11ce-40f4-80f5-99f9f185efb7")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Arrays/PythagoreanTriplet/PythagoreanTriplet/PythagoreanTriplet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.arrays
+{
+    public class PythagoreanTriplet
+    {
+       public static bool HasTriplet(int[] input)
+        {
+            int length = input.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    for (int k = j + 1; k < length; k++)
+                    {
+                        int x = input[i] * input[i], y = input[j] * input[j], z = input[k] * input[k];
+
+                        if (x == y + z || y == x + z || z == x + y)
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+       private bool IsTriplet(int a, int b, int c)
+       {
+           bool first, second, third;
+
+           first = ((a + b) > c);
+           second = ((a + c) > b);
+           third = ((c + b) > a);
+
+           return (first && second && third);
+       }
+    }
+}
