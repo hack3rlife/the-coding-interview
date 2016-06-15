4a2ffb80bf3ddade1280fcef52e82a5704d1d5f8
hack3rlife_cp <SND\hack3rlife_cp>
2016-06-15 06:46:51 +0000

    Re-adding SquareRoot project

---
+++ Math/SquareRoot/SquareRootTest/SquareRootTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace com.hack3rlife.math
+{
+    [TestClass]
+    public class SquareRootTest
+    {
+        [TestMethod]
+        public void SquareRoot_OddNumberTest()
+        {
+            //arrange
+            var number = 9;
+            var expected = 3;
+
+            //act
+            var actual = SquareRoot.Get(number);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void SquareRoot_EvenNumberTest()
+        {
+            //arrange
+            var number = 10;
+            var expected = 3.1622776601683791;
+
+            //act
+            var actual = SquareRoot.Get(number);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
