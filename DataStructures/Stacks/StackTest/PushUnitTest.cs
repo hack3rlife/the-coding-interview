fde0ab43993b0219fd7ac7f39618376772e8a0db
hack3rlife_cp <SND\hack3rlife_cp>
2016-09-09 00:32:21 +0000

    Addming missing elements to the source code

---
+++ DataStructures/Stacks/StackTest/PushUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.hack3rlife.datastructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.hack3rlife.datastructures
+{
+    [TestClass()]
+    public class PushUnitTest
+    {
+        [TestMethod()]
+        public void PushTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ thecodinginterview.all.proj.ps1
@@ -0,0 +1,18 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+$scriptDirectory = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
+$dteObj = New-Object -ComObject "VisualStudio.DTE.14.0"
+
+$slnDir = ".\"
+$slnName = "The Coding Interview"
+
+#$writer = new-object System.IO.StreamWriter ($scriptDirectory, $false, [System.Text.Encoding]::UTF8)
+#$writer.WriteLine("Microsoft Visual Studio Solution File, Format Version 14.00")
+#$writer.WriteLine("Creating solution {0}", $slnName)
+
+$dteObj.Solution.Create($scriptDirectory, $slnName)
+(ls . -Recurse *.csproj) | % { $dteObj.Solution.AddFromFile($_.FullName, $false) }
+
+$dteObj.Solution.SaveAs( (Join-Path $scriptDirectory 'The Coding Interview.sln') ) 
+
+$dteObj.Quit()
