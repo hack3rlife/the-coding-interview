0ca8e645213672acb4ea277ff4711b5b585ade98
hack3rlife <hack3rlife@outlook.com>
2017-05-10 21:29:36 -0700

    1. Adding Jump Game

---
+++ DynamicProgramming/JumpGame/JumpGame/JumpGame.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+
+namespace com.hack3rlife.dynamicprogramming
+{
+    public static class JumpGame
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="currIndex"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static int MinJumps(int[] input, int currIndex, int length)
+        {
+            if (currIndex == length)
+                return 0;
+
+            if (input[currIndex] == 0)
+                return int.MaxValue;
+
+            var min = int.MaxValue;
+
+            for (int i = currIndex + 1; i <= currIndex + input[currIndex] && i <= length; i++)
+            {
+                int currJumps = MinJumps(input, i, length);
+
+                if (currJumps != int.MaxValue && currJumps + 1 < min)
+                    min = currJumps + 1;
+            }
+
+            return min;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int MinJumps(int[] input)
+        {
+            int length = input.Length;
+
+            if (length == 0)
+                return 0;
+
+            int[] jumps = new int[length];
+            int[] jumpsIndex = new int[length];
+
+            for (int i = 1; i < length; i++)
+            {
+                jumps[i] = int.MaxValue;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (i <= j + input[j])
+                    {
+                        if (jumps[i] > jumps[j] + 1)
+                        {
+                            jumps[i] = jumps[j] + 1;
+                            jumpsIndex[i] = j;
+
+                            Print(jumps);
+                        }
+                    }
+                }
+            }
+
+            return jumps[length - 1];
+        }
+
+        /// <summary>
+        /// Print the temporal DPT
+        /// </summary>
+        /// <param name="input">The table</param>
+        private static void Print(int[] input)
+        {
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                Debug.Write(string.Format("| {0} |", input[i]));
+            }
+
+            Debug.WriteLine("");
+        }
+    }
+}
---
+++ DynamicProgramming/JumpGame/JumpGameTest/JumpGameTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace com.hack3rlife.dynamicprogramming.test
+{
+    [TestClass()]
+    public class JumpGameTest
+    {
+        [TestMethod()]
+        public void MinJumps_RecursiveMethod_Test()
+        {
+            //arrange
+            int[] input = new int[] { 2, 3, 1, 1, 4 };
+
+            //act 
+            int actual = JumpGame.MinJumps(input, 0, input.Length - 1);
+
+            //assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod()]
+        public void MinJumps_RecursieMethod_Test()
+        {
+            //arrange
+            //int[] input = new int[] { 2, 3, 1, 1, 4 };
+            //int[] input = new int[] { 1, 3, 6, 1, 0, 9 };
+            int[] input = new int[] { 1, 3, 6, 3, 2, 3, 6, 8, 9, 5 };
+
+            //act 
+            int actual = JumpGame.MinJumps(input);
+
+            //assert
+            Assert.AreEqual(4, actual);
+        }
+    }
+}
